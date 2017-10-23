ba64a167e555ea90fa8840a1f847ca68ee5c4d37
Selebrator <l.panneke@googlemail.com>
2017-10-23 16:40:40 +0200

    Added notifications

--- src/de/selebrator/pwgroup/Main.java
+++ src/de/selebrator/pwgroup/Main.java
@@ -2,8 +2,7 @@ package de.selebrator.pwgroup;
 
 import net.milkbowl.vault.permission.Permission;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -19,7 +18,9 @@ public class Main extends JavaPlugin {
 	private String message_user = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("message_user"));
 	private String message_deny = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("message_deny"));
 	private String message_wrong = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("message_wrong"));
+	private String message_wrong_notify = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("message_wrong_notify"));
 	private String message_success = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("message_success"));
+	private String message_success_notify = ChatColor.translateAlternateColorCodes('&', this.getConfig().getString("message_success_notify"));
 
 	@Override
 	public void onEnable() {
@@ -59,13 +60,37 @@ public class Main extends JavaPlugin {
 				for(String group : this.groups)
 					permission.playerAddGroup(null, player, group);
 				player.sendMessage(this.message_success);
+				String notification = this.message_success_notify
+						.replaceAll("%player_uuid%", player.getUniqueId().toString())
+						.replaceAll("%player_displayname%", player.getDisplayName())
+						.replaceAll("%player_name%", player.getName());
+
+				this.notify(notification, sender, "password.notify.success");
 				return true;
 			} else {
 				player.sendMessage(this.message_wrong);
+				String notification = this.message_wrong_notify
+						.replaceAll("%player_uuid%", player.getUniqueId().toString())
+						.replaceAll("%player_displayname%", player.getDisplayName())
+						.replaceAll("%player_name%", player.getName())
+						.replaceAll("%input%", args[0]);
+
+				this.notify(notification, sender, "password.notify.wrong");
 				return true;
 			}
 		}
 
 		return false;
+	}
+
+	private void notify(String message, CommandSender sender, String permission) {
+		if(!(sender instanceof ConsoleCommandSender))
+			this.getServer().getConsoleSender().sendMessage(message);
+
+		this.getServer().getOnlinePlayers().stream()
+				.filter(player -> Main.permission.has(player, permission))
+				.filter(player -> player != sender)
+				.peek(player -> System.out.println(player.getName()))
+				.forEach(player -> player.sendMessage(message));
 	}
 }
